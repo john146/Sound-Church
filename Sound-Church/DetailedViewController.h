b8c604f8001b6eb5747ef7289b3c8e3192c0b4eb
John Ahrens <john@john-ahrens.com>
2011-07-23 12:51:46 -0700

    New files to add Detailed View where we actually play the podcast.

---
+++ Sound-Church/DetailedViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailedViewController.h
+//  Sound-Church
+//
+//  Created by John Ahrens on 7/23/11.
+//  Copyright 2011 John Ahrens, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DetailedViewController : UIViewController {
+    
+}
+
+@end
